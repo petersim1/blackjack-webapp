c4a70154d1b527f8acf78ea8479b54435f33225e
Peter Simone <plssimone@gmail.com>
2023-11-17 20:19:10 -0500

    add svg assets

--- app/_components/Layout/Footer/settings.tsx
+++ app/_components/Layout/Footer/settings.tsx
@@ -2,13 +2,14 @@
 
 import { useModalContext } from "@/_lib/providers";
 import SettingsModal from "@/_components/Elements/Modal/settings";
+import { Settings } from "@/_assets";
 import styled from "../styled.module.css";
 
 export default (): JSX.Element => {
   const { handleModal } = useModalContext();
   return (
     <div className={styled.settings} onClick={() => handleModal(<SettingsModal />)}>
-      Settings
+      <Settings fill="white" height="1rem" width="1rem" />
     </div>
   );
 };
