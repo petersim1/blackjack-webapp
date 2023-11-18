f39f55e662c5995cb27c794ba7cbd769309eac32
Peter Simone <plssimone@gmail.com>
2023-11-17 20:13:04 -0500

    add settings modal

--- app/_components/Elements/Modal/settings.tsx
+++ app/_components/Elements/Modal/settings.tsx
@@ -1,8 +1,44 @@
 "use client";
 
+import { useState } from "react";
+
+import Toggle from "../Toggle";
+import styled from "../styled.module.css";
+
+const defaultValues = {
+  dealer_hit_soft17: false,
+  push_dealer22: false,
+  double_after_split: true,
+  hit_after_split_aces: false,
+  reduced_blackjack_payout: false,
+  allow_surrender: true,
+  shrink_deck: true,
+};
 
 export default (): JSX.Element => {
+  const [on, setOn] = useState<{ [key: string]: boolean }>({ ...defaultValues });
+
+  const handleClick = (name: string): void => {
+    setOn((prev) => ({ ...prev, [name]: !on[name] }));
+  };
+
+  const handleSubmit = (): void => {
+    console.log(on);
+  };
+
   return (
-    <div></div>
-  )
-}
+    <div className={styled.settings_modal}>
+      <h3>Rules</h3>
+      {Object.entries(on).map(([name, value], ind) => (
+        <div key={ind} className={styled.rule}>
+          <span>{name}</span>
+          <Toggle state={value} setState={() => handleClick(name)} />
+        </div>
+      ))}
+      <button onClick={handleSubmit}>Submit</button>
+      <span className={styled.warning}>
+        Pressing submit will start a new game under the given rules.
+      </span>
+    </div>
+  );
+};
--- app/_components/Layout/Footer/settings.tsx
+++ app/_components/Layout/Footer/settings.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import { useModalContext } from "@/_lib/providers";
+import SettingsModal from "@/_components/Elements/Modal/settings";
 import styled from "../styled.module.css";
-
-const FakeDiv = (): JSX.Element => {
-  return <div style={{ height: "300px", width: "300px" }}>Hello modal</div>;
-};
 
 export default (): JSX.Element => {
   const { handleModal } = useModalContext();
   return (
-    <div className={styled.settings} onClick={() => handleModal(<FakeDiv />)}>
+    <div className={styled.settings} onClick={() => handleModal(<SettingsModal />)}>
       Settings
     </div>
   );
