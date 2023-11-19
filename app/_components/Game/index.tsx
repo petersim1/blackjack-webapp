bc91d6c446ed0deaa5755f7e8db14c4d6b201322
Peter Simone <plssimone@gmail.com>
2023-11-19 13:50:59 -0500

    add logic to reflect cut card in css var

--- app/_components/Game/index.tsx
+++ app/_components/Game/index.tsx
@@ -7,16 +7,17 @@ import House from "./House";
 import Player from "./Player";
 import Options from "./Options";
 
-import { useWsDataContext } from "@/_lib/hooks";
+import { useWsDataContext, useLocalStorage } from "@/_lib/hooks";
 
 export default (): JSX.Element => {
   const { ws, connected, gameData } = useWsDataContext();
+  const { storeData } = useLocalStorage();
 
   return (
     <div className={styled.board}>
       <div className={styled.top_row}>
         <House cards={gameData.data.house_cards} />
-        <Shoe percent={gameData.data.cards_remaining} />
+        <Shoe percent={gameData.data.cards_remaining} cut={storeData.deck.ratio_penetrate} />
       </div>
       <Player cards={gameData.data.player_cards} total={gameData.data.player_total} />
       <Options data={gameData.data} connected={connected} ws={ws} />
