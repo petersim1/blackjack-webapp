9e0b03b5aee6b7547917c926540fb505a3ba978c
Peter Simone <plssimone@gmail.com>
2023-12-13 13:44:39 -0500

    test styling

--- app/_components/Layout/index.tsx
+++ app/_components/Layout/index.tsx
@@ -1,10 +1,22 @@
-import styles from "./styled.module.css";
+// import styles from "./styled.module.css";
 
 import Footer from "./Footer";
 import Sidebar from "./Sidebar";
 
+import { css } from "@/styled-system/css";
+
+const styles = css({
+  display: "grid",
+  gridTemplateColumns: "250px 1fr",
+  gridTemplateRows: "1fr 50px",
+  width: "100%",
+  height: "100vh",
+  minHeight: "100vh",
+  maxHeight: "100vh",
+});
+
 export default ({ children }: { children: React.ReactNode }): JSX.Element => {
-  return <div className={styles.layout}>{children}</div>;
+  return <div className={styles}>{children}</div>;
 };
 
 export { Footer, Sidebar };
